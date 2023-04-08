13f38019440c1fed3a10422b5fbc1dd6c7e5fc00
Tobiasz Harasimowicz <tobiasz.harasimowicz@gmail.com>
2023-04-08 14:16:09 +0200

    feat(#13): Add clikt to read parameters #13
    closes #13

--- src/main/kotlin/app/hsc/Supabase.kt
+++ src/main/kotlin/app/hsc/Supabase.kt
@@ -14,6 +14,7 @@ class Supabase(
         if (client.gotrue.loadFromStorage()) {
             return client.gotrue.sessionStatus.value
         }
+
         client.gotrue.loginWith(Discord)
         return client.gotrue.sessionStatus.value
     }
